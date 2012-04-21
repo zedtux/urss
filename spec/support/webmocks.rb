1bdc6ce86b4ef6a282f86bd57b6db77f1d8206f4
Guillaume Hain <zedtux@zedroot.org>
2012-04-21 19:51:30 +0200

    Updated spec/support/webmocks in order to be compatible with Ree, 1.8.7 and JRuby

--- spec/support/webmocks.rb
+++ spec/support/webmocks.rb
@@ -1,11 +1,4 @@
-stub_request(:get, "http://tech.rufy.com")
-  .to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "rss20.xml")).read, :headers => {})
-
-stub_request(:get, "http://www.ruby-lang.org/en/feeds/news.rss")
-  .to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "ruby.rss")).read, :headers => {})
-
-stub_request(:get, "http://www.flickr.com/photos/herval/")
-  .to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "media_rss.xml")).read, :headers => {})
-
-stub_request(:get, "http://waxluxembourg.com/feed/")
-  .to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "wax.rss")).read, :headers => {})
+stub_request(:get, "http://tech.rufy.com").to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "rss20.xml")).read, :headers => {})
+stub_request(:get, "http://www.ruby-lang.org/en/feeds/news.rss").to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "ruby.rss")).read, :headers => {})
+stub_request(:get, "http://www.flickr.com/photos/herval/").to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "media_rss.xml")).read, :headers => {})
+stub_request(:get, "http://waxluxembourg.com/feed/").to_return(:status => 200, :body => File.open(File.join(File.dirname(__FILE__), "fixtures", "wax.rss")).read, :headers => {})
